2267a29249cd3ef79dc7ba91e048601aade33b33
Jordyn <jkuhn2472@mail.kvcc.edu>
2024-02-02 15:28:16 -0500

    Add Post model. Add ListView for posts. Add basic templates.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    """Post Model Class"""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String method"""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Get the absolute URL for a single post"""
+        return reverse("post_detail", kwargs={"pk":self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+class BlogListView(ListView):
+    """Blog list view"""
 
-# Create your views here.
+    model = Post
+    template_name = "home.html"
